d8d63ace555af63c20d897bf668e11a3a787e9a3
Xoms <cilcksensead@gmail.com>
2020-10-14 08:50:42 +0300

    fainal

--- js/mobile-sandwich.js
+++ js/mobile-sandwich.js
@@ -3,20 +3,17 @@
     const headerBtn = document.querySelector('.top-menu-btn')
     const svgIcon = document.querySelector('.top-menu-btn use');
     let closeFlag = false;
-    console.log(svgIcon);
+    
     headerBtn.addEventListener('click', ()=>{
         console.log('click');
         closeFlag = !closeFlag;
         switchIcon();
-        headerNav.classList.toggle('header__content--sticky')
+        headerNav.classList.toggle('header__content--mobile')
     })
 
     function switchIcon(){
         closeFlag ? svgIcon.setAttribute('href',"./images/sprite.svg#icon-mobile-menu-close") : 
             svgIcon.setAttribute('href',"./images/sprite.svg#icon-mobile-menu");
     }
-    // window.onresize(()=>{
-    //     (parseInt(document.body.clientWidth) >= 768) && 
-    //     headerNav.classList.remove('header__content--sticky');
-    // })
+    
 })()
